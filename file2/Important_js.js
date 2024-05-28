f44afb7d89109f65884c1445f744a53afce82ee3
LalitMaurya3441 <151951932+LalitMaurya3441@users.noreply.github.com>
2024-05-28 09:20:24 +0000

    main

---
+++ file2/Important_js.js
@@ -0,0 +1,12 @@
+/*  JavaScript Excution Content
+
+   ( {} -Global Excution Context (EX-{this})
+    -Function Execution Context
+     -Eval Execution Context )
+
+    (- Memory Creation Phase
+     -Execution Phase)
+
+    
+
+ */
