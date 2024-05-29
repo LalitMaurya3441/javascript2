0a0a385726d10218a455f42b849acd1b3598d80a
LalitMaurya3441 <151951932+LalitMaurya3441@users.noreply.github.com>
2024-05-29 07:48:25 +0000

    main

---
+++ file2/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const week =2;
+
+switch (week) {
+    case 1:
+        console.log("Sunday")
+        break;
+    case 2:
+            console.log("Monday")
+            break;
+
+    default:
+        console,log("Tuesday")
+        break;
+}
+//Nullish Coalescing operator
+
+// const val1 = null ?? 10;
+// console.log(val1);
+
+// ternery operator:
+
+const Roll_no =3441;
+
+(Roll_no>=3442) ? console.log("Lalit") : console.log("Santosh");
